f788e8d3d886433e3c2a9d4800887ce9afd0ce46
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-02 11:34:02 +0200

    Restructure IF in gosia.f

--- gosia.f
+++ gosia.f
@@ -1074,12 +1074,10 @@ C     Treat suboption CONT (control)
                               jpin(ig1) = ig2
                            ENDDO
                            GOTO 350 ! Back to beginning of CONT suboption
+                        ELSEIF ( op1.EQ.'END,' ) THEN
+                          GOTO 2900
                         ELSE
-                           IF ( op1.EQ.'END,' ) THEN
-                             GOTO 2900
-                           ELSE
-                             GOTO 350 ! Back to beginning of CONT suboption
-                           ENDIF
+                          GOTO 350 ! Back to beginning of CONT suboption
                         ENDIF
                      ENDIF
                   ENDIF
