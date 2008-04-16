21453eba7cbe1734aa5d73d72e1b978ca0cdbdb5
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-16 14:48:44 +0200

    Comments added

--- mixr.f
+++ mixr.f
@@ -7,16 +7,16 @@ C
 C Purpose: calculate theoretical mixing ratio and compare to experimental one.
 C
 C Uses global variables:
-C      DMIX   -
+C      DMIX   - 0.8326 * gamma energy
 C      DMIXE  - mixing ratio and its error
-C      IMIX   -
+C      IMIX   - matrix element associated with known mixing ratio
 C      KSEQ   - index into ELM for pair of levels, and into EN or SPIN
 C      LNY    - use logs to calculate chi squared
 C      NDL    - number of mixing ratios
 C
 C Formal parameters:
 C      Nw     - number of data points used to calculate chi squared
-C      Ipsw   -
+C      Ipsw   - printing flag (0 means no print, 1 means print)
 C      Chi    - chi squared
 C      Chilo  - chi squared using logs
  
@@ -33,8 +33,9 @@ C      Chilo  - chi squared using logs
 
       IF ( NDL.EQ.0 ) RETURN
       Nw = Nw + NDL
-      DO i = 1 , NDL
-         it = IMIX(i)
+
+      DO i = 1 , NDL ! For each mixing ratio
+         it = IMIX(i) ! Matrix element for this mixing ratio
          inx = KSEQ(it,1) ! Index 1 of it'th decay
          inx1 = KSEQ(it,2) ! Index 2 of it'th decay
          IF ( ABS(ELM(inx1)).LT.1.E-5 ) ELM(inx1) = 1.E-5
@@ -44,16 +45,20 @@ C      Chilo  - chi squared using logs
          IF ( LNY.EQ.1 ) Chilo = Chilo + 
      &                           (DMIXE(i,1)*LOG(ABS(dl/DMIXE(i,1)))
      &                           /DMIXE(i,2))**2
-      ENDDO
+      ENDDO ! Loop on mixing ratios i
+
       IF ( Ipsw.EQ.0 ) RETURN
+      
       WRITE (22,99001)
 99001 FORMAT (1X//10X,'E2/M1 MIXING RATIOS'/10X,'TRANSITION',10X,
      &        'EXP.DELTA',10X,'CALC.DELTA',10X,'SIGMA'/)
-      DO i = 1 , NDL
-         dl = (DMIX(i)-DMIXE(i,1))/DMIXE(i,2)
-         it = IMIX(i)
+
+      DO i = 1 , NDL ! For each mixing ratio
+         dl = (DMIX(i)-DMIXE(i,1))/DMIXE(i,2) ! Relative error
+         it = IMIX(i) ! Matrix element for this mixing ratio
          WRITE (22,99002) KSEQ(it,3) , KSEQ(it,4) , DMIXE(i,1) , DMIX(i)
      &                    , dl ! KSEQs are level numbers
 99002    FORMAT (10X,1I2,'---',1I2,14X,1F7.2,12X,1F7.2,13X,1F5.2)
-      ENDDO
+      ENDDO ! Loop on mixing ratios i
+
       END
