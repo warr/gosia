6c8ce10047baa06a1c4e242cb4a35780a95ba2eb
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-17 07:37:11 +0200

    Comments added

--- newcat.f
+++ newcat.f
@@ -41,13 +41,14 @@ C     Jidim   -
       Jidim = NMAX + 1
       IF ( MAGA(Iexp).NE.0 ) Jidim = 3*NMAX + 1
       ist = 1
-      DO kk = 1 , 6
-         IF ( MULTI(kk).NE.0 ) THEN
-            istop = MULTI(kk) - 1 + ist
-            DO k = ist , istop
+      DO kk = 1 , 6 ! For each multipolarity E1 to E6
+         IF ( MULTI(kk).NE.0 ) THEN ! If there are matrix elements for this multipolarity
+            istop = MULTI(kk) - 1 + ist ! Last matrix element for this multipolarity
+            DO k = ist , istop ! For each matrix element for this multipolarity
                xx = ABS(XI(k))
                xx = xx/XIR(kk,Iexp)
-               DO n = 1 , 7 , 3
+
+               DO n = 1 , 7 , 3 ! For n in 1, 4 and 7
                   IF ( MAGA(Iexp).NE.0 .OR. n.EQ.4 ) THEN
                      zt = QAPR(k,1,n)
                      zt = ABS(zt)
@@ -62,17 +63,18 @@ C     Jidim   -
                      a = wg*PARXM(Iexp,3,ng,kk) + wl*PARXM(Iexp,3,nl,kk)
                      b = wg*PARXM(Iexp,4,ng,kk) + wl*PARXM(Iexp,4,nl,kk)
                      q2 = a*zt + b
-                     QAPR(k,2,n) = QAPR(k,1,n)*q2*FXIS2(k,n)
-                     QAPR(k,1,n) = QAPR(k,1,n)*q1*FXIS1(k,n)
+                     QAPR(k,2,n) = QAPR(k,1,n)*q2*FXIS2(k,n) ! FXIS2 = 1
+                     QAPR(k,1,n) = QAPR(k,1,n)*q1*FXIS1(k,n) ! FXIS1 = sign(xi)
                      IF ( IAPR(k,1).EQ.IAPR(k,2) ) THEN
                         QAPR(k,1,n) = 0.
                         QAPR(k,2,n) = QAPR(k,2,n)/2.
                      ENDIF
                   ENDIF
-               ENDDO
+               ENDDO ! Loop over n
+
                IF ( MAGA(Iexp).NE.0 ) THEN
                   DO n = 2 , 6
-                     IF ( n.NE.4 ) THEN
+                     IF ( n.NE.4 ) THEN ! For N in 2, 3, 5, 6
                         zt = QAPR(k,1,n)
                         zt = ABS(zt)
                         xp = 4.*xx
@@ -84,13 +86,13 @@ C     Jidim   -
      &                       + wl*PARX(Iexp,2*kk-1,nl)
                         q2 = wg*PARX(Iexp,2*kk,ng)
      &                       + wl*PARX(Iexp,2*kk,nl)
-                        QAPR(k,2,n) = QAPR(k,1,n)*q2*FXIS2(k,n)
-                        QAPR(k,1,n) = QAPR(k,1,n)*q1*FXIS1(k,n)
+                        QAPR(k,2,n) = QAPR(k,1,n)*q2*FXIS2(k,n) ! FXIS2 = sign(xi)
+                        QAPR(k,1,n) = QAPR(k,1,n)*q1*FXIS1(k,n) ! FXIS1 = 1
                      ENDIF
-                  ENDDO
+                  ENDDO ! Loop over n
                ENDIF
-            ENDDO
+            ENDDO ! Loop over matrix elements for this mult. k
             ist = istop + 1
          ENDIF
-      ENDDO
+      ENDDO ! Loop over electric multipolarities kk
       END
