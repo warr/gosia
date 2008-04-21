180ef8344320cbbcdc6c277343c37b5119cf5940
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-21 12:15:03 +0200

    Move OP,ERRO code out of main loop

--- gosia.f
+++ gosia.f
@@ -832,64 +832,7 @@ C     Treat OP,RE,C (release C)
 
 C     Treat OP,ERRO (calculate errors)
       ELSEIF ( op2.EQ.'ERRO' ) THEN
-         READ * , idf , ms , mend , irep , ifc , remax
-         rem = LOG(remax)
-         LOCKS = 0
-         LOCKF = 0
-         JENTR = 1 ! Flag to indicate we are in OP,ERRO
-         sh = 1.
-         ifbp = 0
-         inpo = 1
-         inko = 1
-         IF ( iosr.NE.0 .AND. idf.NE.0 ) THEN
-            inn = 0
-            ij = MULTI(1)
-            IF ( ij.NE.0 ) THEN
-               DO ij = 1 , NMAX
-                  lxd = LDNUM(1,ij)
-                  IF ( lxd.NE.0 ) THEN
-                     DO ijk = 1 , lxd
-                        inn = inn + 1
-                     ENDDO
-                  ENDIF
-               ENDDO
-               inpo = inn + 1
-            ENDIF
-            DO ij = 1 , NMAX
-               lxd = LDNUM(2,ij)
-               IF ( lxd.NE.0 ) THEN
-                  DO ijk = 1 , lxd
-                     inn = inn + 1
-                  ENDDO
-               ENDIF
-            ENDDO
-            inko = inn
-            IF ( irep.NE.2 ) THEN
-               WRITE (3,*) NMAX , MEMAX , inpo , inko
-               DO inn = 1 , NMAX
-                  WRITE (3,*) inn , SPIN(inn) , EN(inn)
-               ENDDO
-               DO inn = 1 , MEMAX
-                  WRITE (3,*) inn , LEAD(1,inn) , LEAD(2,inn)
-               ENDDO
-               DO inn = 1 , MEMAX
-                  WRITE (3,*) inn , ELM(inn)
-               ENDDO
-            ENDIF ! IF ( irep.NE.2 )
-         ENDIF ! IF ( iosr.NE.0 .AND. idf.NE.0 )
-         IF ( irep.NE.0 ) THEN
-            REWIND 15
-            READ (15,*) (DEVD(kh1),DEVU(kh1),kh1=1,MEMAX)
-         ELSE
-            DO kh1 = 1 , MEMAX
-               DEVD(kh1) = ELML(kh1) - ELM(kh1)
-               DEVU(kh1) = ELMU(kh1) - ELM(kh1)
-            ENDDO
-         ENDIF
-         IF ( IMIN.EQ.0 ) CALL CMLAB(0,dsig,ttttt)
-         IF ( ERR ) GOTO 2000 ! Error
-         IF ( IMIN.NE.0 ) GOTO 400
-         GOTO 1300 ! End of OP,ERRO
+         GOTO 3600
 
 C     Treat OP,TITL (title)
       ELSEIF ( op2.EQ.'TITL' ) THEN
@@ -2794,6 +2737,67 @@ C     Handle OP,RE,*
          ivarh(jrls) = IVAR(jrls)
       ENDDO ! Loop on matrix elements jrls
       GOTO 100 ! Back to input loop
+
+C.............................................................................
+C     Handle OP,ERRO
+ 3600 READ * , idf , ms , mend , irep , ifc , remax
+      rem = LOG(remax)
+      LOCKS = 0
+      LOCKF = 0
+      JENTR = 1 ! Flag to indicate we are in OP,ERRO
+      sh = 1.
+      ifbp = 0
+      inpo = 1
+      inko = 1
+      IF ( iosr.NE.0 .AND. idf.NE.0 ) THEN
+         inn = 0
+         ij = MULTI(1)
+         IF ( ij.NE.0 ) THEN
+            DO ij = 1 , NMAX
+               lxd = LDNUM(1,ij)
+               IF ( lxd.NE.0 ) THEN
+                  DO ijk = 1 , lxd
+                     inn = inn + 1
+                  ENDDO
+               ENDIF
+            ENDDO
+            inpo = inn + 1
+         ENDIF
+         DO ij = 1 , NMAX
+            lxd = LDNUM(2,ij)
+            IF ( lxd.NE.0 ) THEN
+               DO ijk = 1 , lxd
+                  inn = inn + 1
+               ENDDO
+            ENDIF
+         ENDDO
+         inko = inn
+         IF ( irep.NE.2 ) THEN
+            WRITE (3,*) NMAX , MEMAX , inpo , inko
+            DO inn = 1 , NMAX
+               WRITE (3,*) inn , SPIN(inn) , EN(inn)
+            ENDDO
+            DO inn = 1 , MEMAX
+               WRITE (3,*) inn , LEAD(1,inn) , LEAD(2,inn)
+            ENDDO
+            DO inn = 1 , MEMAX
+               WRITE (3,*) inn , ELM(inn)
+            ENDDO
+         ENDIF ! IF ( irep.NE.2 )
+      ENDIF ! IF ( iosr.NE.0 .AND. idf.NE.0 )
+      IF ( irep.NE.0 ) THEN
+         REWIND 15
+         READ (15,*) (DEVD(kh1),DEVU(kh1),kh1=1,MEMAX)
+      ELSE
+         DO kh1 = 1 , MEMAX
+            DEVD(kh1) = ELML(kh1) - ELM(kh1)
+            DEVU(kh1) = ELMU(kh1) - ELM(kh1)
+         ENDDO
+      ENDIF
+      IF ( IMIN.EQ.0 ) CALL CMLAB(0,dsig,ttttt)
+      IF ( ERR ) GOTO 2000 ! Error
+      IF ( IMIN.NE.0 ) GOTO 400
+      GOTO 1300 ! End of OP,ERRO
 
 C.............................................................................
 
