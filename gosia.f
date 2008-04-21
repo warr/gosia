02a31993c971a692fc7feabf7158be6aa7f2d66a
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-21 12:19:16 +0200

    Move OP,THEO code out of main loop

--- gosia.f
+++ gosia.f
@@ -838,6 +838,10 @@ C     Treat OP,TITL (title)
       ELSEIF ( op2.EQ.'TITL' ) THEN
          GOTO 3700
 
+C     Treat OP,THEO
+      ELSEIF ( op2.EQ.'THEO' ) THEN
+         GOTO 3800
+
 C     Treat OP,GOSI
       ELSEIF ( op2.EQ.'GOSI' ) THEN
          oph = op2
@@ -860,69 +864,6 @@ C     Treat OP,MINI
          IMIN = IMIN + 1
          IF ( IMIN.NE.1 ) GOTO 1400
          GOTO 1200 ! End of OP,MINI
-
-C     Treat OP,THEO
-      ELSEIF ( op2.EQ.'THEO' ) THEN
-         REWIND (12)
-         ibaf = 1
-         DO jb = 1 , LP1 ! LP1 = 50 (maximum number of experiments)
-            DO lb = 1 , 2
-               xlevb(jb,lb) = 0
-            ENDDO
-         ENDDO
-         READ * , nbands ! Number of bands
-         IF ( nbands.LE.0 ) ibaf = 0
-         nbands = ABS(nbands)
-         DO nl = 1 , 8
-            DO jb = 1 , nbands
-               DO jl = 1 , nbands
-                  DO kl = 1 , 3
-                     bm(nl,jb,jl,kl) = 0.
-                  ENDDO
-               ENDDO
-            ENDDO
-         ENDDO
-         DO jb = 1 , nbands
-            READ * , bk , ilevls ! K of band, number of levels in band
-            READ * , (levl(ib),ib=1,ilevls) ! Level list for band
-            DO kb = 1 , ilevls
-               inva = levl(kb)
-               xlevb(inva,2) = bk
-               xlevb(inva,1) = DBLE(jb)
-            ENDDO
-         ENDDO
-         DO nl = 1 , 8
-            READ * , nnl ! Multipolarity
- 126        IF ( nnl.LE.0 ) GOTO 130
-            READ * , jb1 , jb2 ! band indices
-            IF ( jb1.NE.0 ) THEN
-               READ * , (bm(nnl,jb1,jb2,j),j=1,3) ! intrinsic moments
-               DO j = 1 , 3
-                  bm(nnl,jb2,jb1,j) = bm(nnl,jb1,jb2,j)
-               ENDDO
-               GOTO 126
-            ENDIF
-         ENDDO
- 130     DO kb = 1 , MEMAX
-            IF ( ibaf.NE.0 ) THEN
-               ind1 = LEAD(1,kb)
-               ind2 = LEAD(2,kb)
-               xi1 = SPIN(ind1)
-               xi2 = SPIN(ind2)
-               lamd = mlt(kb)
-               nb1 = INT(xlevb(ind1,1)+.1)
-               nb2 = INT(xlevb(ind2,1)+.1)
-               xk1 = xlevb(ind1,2)
-               xk2 = xlevb(ind2,2)
-               xm1 = bm(lamd,nb1,nb2,1)
-               xm2 = bm(lamd,nb1,nb2,2)
-               xm3 = bm(lamd,nb1,nb2,3)
-               ELM(kb) = ELMT(xi1,xi2,lamd,nb1,nb2,xk1,xk2,xm1,xm2,xm3)
-               IF ( ABS(ELM(kb)).LT.1E-6 ) ELM(kb) = 1.E-6
-               WRITE (12,*) ELM(kb)
-            ENDIF
-         ENDDO
-         GOTO 100 ! End of OP,THEO - back to input loop
 
 C     Treat OP,YIEL
       ELSEIF ( op2.EQ.'YIEL' ) THEN
@@ -2804,6 +2745,68 @@ C     Handle OP,TITL
       GOTO 100 ! End of OP,TITL - back to input loop
 
 C.............................................................................
+C     Handle OP,THEO
+ 3800 REWIND (12)
+      ibaf = 1
+      DO jb = 1 , LP1 ! LP1 = 50 (maximum number of experiments)
+         DO lb = 1 , 2
+            xlevb(jb,lb) = 0
+         ENDDO
+      ENDDO
+      READ * , nbands ! Number of bands
+      IF ( nbands.LE.0 ) ibaf = 0
+      nbands = ABS(nbands)
+      DO nl = 1 , 8
+         DO jb = 1 , nbands
+            DO jl = 1 , nbands
+               DO kl = 1 , 3
+                  bm(nl,jb,jl,kl) = 0.
+               ENDDO
+            ENDDO
+         ENDDO
+      ENDDO
+      DO jb = 1 , nbands
+         READ * , bk , ilevls ! K of band, number of levels in band
+         READ * , (levl(ib),ib=1,ilevls) ! Level list for band
+         DO kb = 1 , ilevls
+            inva = levl(kb)
+            xlevb(inva,2) = bk
+            xlevb(inva,1) = DBLE(jb)
+         ENDDO
+      ENDDO
+      DO nl = 1 , 8
+         READ * , nnl ! Multipolarity
+ 126     IF ( nnl.LE.0 ) GOTO 130
+         READ * , jb1 , jb2 ! band indices
+         IF ( jb1.NE.0 ) THEN
+            READ * , (bm(nnl,jb1,jb2,j),j=1,3) ! intrinsic moments
+            DO j = 1 , 3
+               bm(nnl,jb2,jb1,j) = bm(nnl,jb1,jb2,j)
+            ENDDO
+            GOTO 126
+         ENDIF
+      ENDDO
+ 130  DO kb = 1 , MEMAX
+         IF ( ibaf.NE.0 ) THEN
+            ind1 = LEAD(1,kb)
+            ind2 = LEAD(2,kb)
+            xi1 = SPIN(ind1)
+            xi2 = SPIN(ind2)
+            lamd = mlt(kb)
+            nb1 = INT(xlevb(ind1,1)+.1)
+            nb2 = INT(xlevb(ind2,1)+.1)
+            xk1 = xlevb(ind1,2)
+            xk2 = xlevb(ind2,2)
+            xm1 = bm(lamd,nb1,nb2,1)
+            xm2 = bm(lamd,nb1,nb2,2)
+            xm3 = bm(lamd,nb1,nb2,3)
+            ELM(kb) = ELMT(xi1,xi2,lamd,nb1,nb2,xk1,xk2,xm1,xm2,xm3)
+            IF ( ABS(ELM(kb)).LT.1E-6 ) ELM(kb) = 1.E-6
+            WRITE (12,*) ELM(kb)
+         ENDIF
+      ENDDO
+      GOTO 100 ! End of OP,THEO - back to input loop
+
 
 99048 FORMAT (1X//50X,'CALCULATED YIELDS'//5X,'EXPERIMENT ',1I2,2X,
      &        'DETECTOR ',1I2/5X,'ENERGY ',1F10.3,1X,'MEV',2X,'THETA ',
