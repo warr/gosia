4f96a5a411849cc2c0096f62af6ca0976849f1a5
Nigel Warr <warr@ikp.uni-koeln.de>
2008-04-25 08:41:47 +0200

    Increase ivari dimensions from 500 to 1500 in gosia.f

--- gosia.f
+++ gosia.f
@@ -494,7 +494,7 @@ C      ZV     - energy meshpoints
      &          tau2(10,7) , xl1(7) , qui(8,10) , cf(8,2) , 
      &          ivarh(1500) , liscl(200) , dsxm(100,100,100) , 
      &          levl(50) , xlevb(50,2) , bm(8,20,20,3) , mlt(1500) , 
-     &          ivari(500) , jpin(50)
+     &          ivari(1500) , jpin(50)
       COMMON /CLUST / ICLUST(50,200) , LASTCL(50,20) , SUMCL(20,500) , 
      &                IRAWEX(50)
       COMMON /CCCDS / NDST(50)
