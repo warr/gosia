156689f256b140612d2758ed5faa2ab8e33fde84
Nigel Warr <warr@ikp.uni-koeln.de>
2008-05-15 16:58:40 +0200

    New version of CONV which reads coefficients from a file

--- conv.f
+++ conv.f
@@ -8,11 +8,6 @@ C
 C Purpose: calculate the conversion coefficient at a particular energy by
 C interpolating over the values provided by the user.
 C
-C Uses global variables:
-C      CC     - conversion coefficients
-C      EG     - energies for conversion coefficients
-C      NICC   - number of conversion coefficients
-C
 C Formal parameters:
 C      Ega    - gamma energy
 C      N      - multipolarity N=1,2,3 = E1,2,3 and N=4,5 = M1,2 (not as elsewhere!)
@@ -22,36 +17,41 @@ C      conversion coefficient interpolated to energy Ega
 
       REAL*8 FUNCTION CONV(Ega,N)
       IMPLICIT NONE
-      REAL*8 AGELI , CC , cpo , cpo1 , cv , EG , Ega , Q
-      INTEGER*4 j , N , n1 , NANG , nen , NICC
-      INTEGER*4 ISPL ! Added for spline
-      DIMENSION cpo(51) , cpo1(51)
-      COMMON /CCC   / EG(50) , CC(50,5) , AGELI(50,200,2) , Q(3,200,8) , 
-     &                NICC , NANG(200) , ISPL
 
-      IF ( N.EQ.0 ) THEN ! If no multipolarity defined
+      INTEGER*4 isfirst, i, j, N, nenergies
+      DATA isfirst/1/
+      REAL*8 energies(1500), bricc(1500, 5), Ega
+      SAVE energies, bricc, isfirst, nenergies
+
+C     The first time, we need to read the data
+      IF ( isfirst.eq.1 ) THEN
+        isfirst = 0
+        DO nenergies = 1, 1500
+          READ(29,*,END=100) energies(nenergies),
+     &      (bricc(nenergies,j),j=1,5)
+        ENDDO
+      ENDIF
+
+C     Check multipolarity is valid
+ 100  IF ( N.LT.1.OR.N.GT.5 ) THEN
          CONV = 0.0
-      ELSEIF ( ABS(CC(1,N)).LT.1.E-9 ) THEN ! If no conversion coefficients given for this multipolarity
-         CONV = 0.0
-      ELSE
-         nen = 4
-         DO j = 1 , NICC ! Loop over coefficients provided by user
-            IF ( Ega.LE.EG(j) ) GOTO 50
-         ENDDO
- 50      n1 = j - 2
-         IF ( n1.LT.1 ) n1 = 1
-         IF ( (j+1).GT.NICC ) n1 = n1 - 1
-         IF ( NICC.LE.4 ) THEN
-            n1 = 1
-            nen = NICC
-         ENDIF
-         DO j = 1 , nen
-            cpo(j) = CC(n1+j-1,N)
-            cpo1(j) = EG(n1+j-1)
-         ENDDO
-C        Interpolate 
-         CALL LAGRAN(cpo1,cpo,4,1,Ega,cv,2,1)
-         CONV = cv
          RETURN
       ENDIF
+
+C     Search for the energy in the list
+
+      DO i = 1, nenergies
+        IF (ABS(Ega - energies(i)) .LT. 1E-3) THEN
+           CONV = bricc(i,N)
+           return
+        ENDIF
+      ENDDO
+
+C     We get here if the energy isn't in the list, so stop with an error
+C     message
+      WRITE (*,'(A,F7.3,A)')
+     & 'Unable to find conversion coefficients for ',
+     &  Ega, ' MeV'
+      STOP 'Missing conversion coefficients'
+
       END
