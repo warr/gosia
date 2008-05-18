d849e77b2de358a255c2513587b5fb9e45483ee3
Nigel Warr <warr@ikp.uni-koeln.de>
2008-05-18 14:23:12 +0200

    Add spline support

--- Makefile
+++ Makefile
@@ -101,6 +101,9 @@ OBJS += select.o
 OBJS += setin.o
 OBJS += simin.o
 OBJS += sixel.o
+OBJS += spline.o
+OBJS += splint.o
+OBJS += splner.o
 OBJS += snake.o
 OBJS += stamp.o
 OBJS += sting.o
@@ -149,5 +152,6 @@ single_file:
 	simin.f mixup.f fxis1.f fxis2.f podziel.f klopot.f \
 	mixr.f coord.f chmem.f pticc.f rndm.f kontur.f rk4.f \
 	qfit.f gamatt.f gcf.f tcexp.f tcabs.f tasin.f tacos.f \
-	openf.f effix.f adhoc.f elmt.f select.f > $(SINGLE_FILE)
+	openf.f effix.f adhoc.f elmt.f select.f splner.f spline.f \
+	splint.f > $(SINGLE_FILE)
 
---
+++ splint.f
@@ -0,0 +1,44 @@
+      SUBROUTINE SPLINT(Xa,Ya,Y2a,N,Xx,Yy)
+c
+c xa,ya - tabulated function
+c n - number of tabulated points
+c y2a - second derivatives vector
+c xx - interpolated point
+c yy - intetpoalted value
+c
+      IMPLICIT NONE
+      INTEGER*4 N
+      REAL*8 Xx , Yy , Xa(*) , Y2a(*) , Ya(*)
+      INTEGER*4 k , khi , klo
+      REAL*8 a , b , h
+      DATA k/0/
+ 
+      klo = 1
+      khi = N
+ 100  IF ( khi-klo.GT.1 ) THEN
+         k = (khi+klo)/2
+         IF ( Xa(k).GT.Xx ) THEN
+            khi = k
+         ELSE
+            klo = k
+         ENDIF
+         GOTO 100
+      ENDIF
+      h = Xa(khi) - Xa(klo)
+C
+C  Linear extrapolation
+C
+      IF ( ABS(h).LT.1E-9 ) THEN
+         IF ( Xx.LT.Xa(1) ) h = 1
+         IF ( Xx.GT.Xa(N) ) k = N - 1
+         a = (Ya(k)-Ya(k+1))/(Xa(k)-Xa(k+1))
+         b = (Ya(k)+Ya(k+1)-a*(Xa(k)+Xa(k+1)))*.5
+         Yy = a*Xx + b
+c         PRINT * , 'splint' , Xx , Yy , 'extrapolation'
+         RETURN
+      ENDIF
+      a = (Xa(khi)-Xx)/h
+      b = (Xx-Xa(klo))/h
+      Yy = a*Ya(klo) + b*Ya(khi) + ((a**3-a)*Y2a(klo)+(b**3-b)*Y2a(khi))
+     &     *(h**2)/6.
+      END
