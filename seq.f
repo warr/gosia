d900517e97c397d416ac80a0016162cafffc3f0c
Nigel Warr <warr@ikp.uni-koeln.de>
2008-06-05 09:30:11 +0200

    Whitespace change

--- seq.f
+++ seq.f
@@ -37,8 +37,7 @@ C 8 = M2.
  
       SUBROUTINE SEQ(Idr)
       IMPLICIT NONE
-      REAL*8 CONV , ega , egs , emax , F , GF , spinf , 
-     &       spini , twoi
+      REAL*8 CONV , ega , egs , emax , F , GF , spinf , spini , twoi
       INTEGER*4 idecay , Idr , indx , inx , inx1 , ir , is , istr1 , 
      &          istr2 , j , js , jsave , k , kpa , l , la , la1 , 
      &          ld , LEADF
