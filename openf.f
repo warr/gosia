de4825a4cbf48c1385c82b85a255d24923518052
Nigel Warr <warr@ikp.uni-koeln.de>
2008-06-05 15:20:58 +0200

    Synchronize openf with gosia2

--- openf.f
+++ openf.f
@@ -6,6 +6,9 @@ C Called by: GOSIA
 C
 C Purpose: open files to specified units.
 C
+C Uses global variables:
+C      JZB    - unit to read from
+C
 C The function reads three integers, the first of which is the unit to use for
 C the open statement. The second is 1 if the file is required to exist already,
 C 2 if it is required not to exist and 3 if it does not matter. The third is 1
@@ -19,14 +22,14 @@ C zero, the function returns. It keeps looping until a unit zero is reached.
       CHARACTER name*60 , opt1*20 , opt2*20
       INCLUDE 'switch1.inc'
 
- 100  READ * , i , j , k ! unit, old/new/unknown, formatted/unformatted
+ 100  READ (JZB,*) i , j , k ! unit, old/new/unknown, formatted/unformatted
       IF ( i.EQ.0 ) RETURN
       IF ( j.EQ.1 ) opt1 = 'OLD'
       IF ( j.EQ.2 ) opt1 = 'NEW'
       IF ( j.EQ.3 ) opt1 = 'UNKNOWN'
       IF ( k.EQ.1 ) opt2 = 'FORMATTED'
       IF ( k.EQ.2 ) opt2 = 'UNFORMATTED'
-      READ 99001 , name ! name of file
+      READ (JZB,99001) name ! name of file
 99001 FORMAT (A)
 
 C     Now open the file
