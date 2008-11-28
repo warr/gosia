e69d061b942ae660ef9dd45e99ae939022b8b0fa
Nigel Warr <warr@ikp.uni-koeln.de>
2008-11-28 11:47:54 +0100

    Comment in cmlab.f

--- cmlab.f
+++ cmlab.f
@@ -32,7 +32,7 @@ C
 C Formal parameters:
 C      Ii     - experiment number (or zero for all experiments)
 C      Dsig   - dsigma
-C      Tetrn  - theta of recoiling nucleus
+C      Tetrn  - theta of recoiling nucleus (in radians)
 
       SUBROUTINE CMLAB(Ii,Dsig,Tetrn)
       IMPLICIT NONE
