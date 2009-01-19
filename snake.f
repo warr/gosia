439911136d1bb7f3aaf92dc6cdb807fb396abf74
Nigel Warr <warr@ikp.uni-koeln.de>
2009-01-19 08:37:33 +0100

    Add Adam's code as a debugging option

--- snake.f
+++ snake.f
@@ -126,3 +126,79 @@ C     Calculate some parameters, which we will pass to QE or QM
       ENDIF
       GOTO 100
       END
+CDEBUG
+CDEBUGC----------------------------------------------------------------------
+CDEBUG
+CDEBUG      subroutine spitq(ixpt,mult)
+CDEBUG      integer*4 ixpt,mult
+CDEBUG      REAL*8 ZETA
+CDEBUG      INTEGER*4 LZETA
+CDEBUG      COMMON /CCOUP / ZETA(50000) , LZETA(8)
+CDEBUG      REAL*8 EPS, EROOT, FIEX
+CDEBUG      INTEGER*4 IEXP, IAXS
+CDEBUG      COMMON /KIN   / EPS(50) , EROOT(50) , FIEX(50,2) , IEXP , IAXS(50)
+CDEBUG      INTEGER*4 LOCQ
+CDEBUG      COMMON /ALLC  / LOCQ(8,7)
+CDEBUG      INTEGER*4 IRA , MAXLA
+CDEBUG      COMMON /RNG   / IRA(8) , MAXLA
+CDEBUG      integer*4 mimmex
+CDEBUG      INTEGER*4 ibm , icm , icnt , idm , irl  , k , lloc , 
+CDEBUG     &           nind , nlm
+CDEBUG      DIMENSION lloc(8) , irl(8)
+CDEBUG      INTEGER*4 LP1 , LP2 , LP3 , LP4 , LP6 , LP7 , LP8 , LP9 , 
+CDEBUG     &          LP10 , LP11 , LP12 , LP13 , LP14
+CDEBUG      COMMON /MGN   / LP1 , LP2 , LP3 , LP4 , LP6 , LP7 , LP8 , LP9 , 
+CDEBUG     &                LP10 , LP11 , LP12 , LP13 , LP14
+CDEBUG      real*8 collfunc,w0
+CDEBUG
+CDEBUGc     Passed in the experiment number (ixpt) and the multipolarity (mult).
+CDEBUGc     I am doing only for electric for now.
+CDEBUGc     This routine is to read the electric collision functions from the ZETA array and print
+CDEBUGc     them to the standard output file (23)  
+CDEBUG
+CDEBUGC     Borrowed this line from subroutine below.
+CDEBUG      w0 = IRA(MAXLA)*.03 + .03 ! Maximum omega to calculate for (steps of 0.03)
+CDEBUG
+CDEBUGc     Look at how LAIAMP uses the ZETA array.
+CDEBUGc     Are these the collision functions?
+CDEBUG
+CDEBUG      write(22,72072)ixpt,mult
+CDEBUG72072 format('The *stored* collision functions ',
+CDEBUG     &       'for experiment ',i2,' and mult E',i1)
+CDEBUG      write(22,71972)
+CDEBUG71972 format('|--------mu=1----------|--------mu=2---------',
+CDEBUG     &       '|--------mu=2...')
+CDEBUG      write(22,71672)
+CDEBUG71672 format('|icnt nind  Qe         |icnt nind  Qe        ',
+CDEBUG     &       '|icnt nind  Qe...')
+CDEBUG      icnt = 0
+CDEBUG 100  icnt = icnt + 1
+CDEBUG
+CDEBUG
+CDEBUG      mimmex = mult + 1
+CDEBUGc     write(22,71772)mimmex
+CDEBUG
+CDEBUGC     With QRANGE I think I am checking that icnt is still in range to index the QE values
+CDEBUGc     for this multipolarity and experiment.  There must be only one epsilon 
+CDEBUGc     value per experiment.
+CDEBUG      CALL QRANGE(icnt,nlm,lloc,ibm,icm,idm,irl)
+CDEBUGc     write(22,71772)mimmex
+CDEBUG71772 format(i4)
+CDEBUGc     test to here
+CDEBUG
+CDEBUG      IF ( nlm.EQ.0 ) RETURN
+CDEBUG
+CDEBUG      DO k = 1 , mimmex
+CDEBUG         nind = LOCQ(mult,k) + icnt
+CDEBUG         collfunc = ZETA(nind+LP7)  ! These are the collision functions
+CDEBUG         write(22,71572)icnt,nind,collfunc
+CDEBUG71572    format(1x,i3,1x,i5,1x,d11.4,$)
+CDEBUG      ENDDO
+CDEBUG      write(22,71872)
+CDEBUG71872 format('')
+CDEBUG      goto 100 
+CDEBUG
+CDEBUG      end
+CDEBUG
+CDEBUG
+CDEBUG
