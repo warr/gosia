40d16a612ab1f8b12eeccea5ad5d03c5d282f424
Nigel Warr <warr@ikp.uni-koeln.de>
2009-03-19 11:39:42 +0100

    Trint and pol4

--- pol4.f
+++ pol4.f
@@ -19,7 +19,5 @@ C Evaluate C0 * A^4 + C1 * A^3 + C2 * A^2 + C3 * A + C4
       IMPLICIT NONE
       REAL*8 A , C0 , C1 , C2 , C3 , C4
 
-      POL4 = 1.
-      IF ( ABS(A).GT.1.E+9 ) RETURN
       POL4 = C4 + A*(C3+A*(C2+A*(C1+A*C0)))
       END
