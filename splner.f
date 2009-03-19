95dc081ba27b9e67d0846fcc3c1c736e50236b1c
Nigel Warr <warr@ikp.uni-koeln.de>
2009-03-19 12:08:49 +0100

    Update splner.f

--- splner.f
+++ splner.f
@@ -12,7 +12,7 @@ C      Yr     - y-coordinate of input data
 C      N      - number of data points
 C      Xx     - value for which to interpolate
 C      Yy     - result of interpolation
-C      Iscal  - mode: 1 = linear, 2 = exponential, 3 = square root
+C      Iscal  - mode: 1 = linear, 2 = logarithmic, 3 = square root
 C
 C Note that the effect of FUNC and FUNC1 depends on Iscal:
 C Iscal = 1   FUNC(y) = y        FUNC1(y) = y
@@ -27,18 +27,31 @@ C
       REAL*8 yp1 , ypn , y(1500) , ys
       INTEGER*4 i , Iscal
       REAL*8 Xx , Yy
- 
+
+C     We need at least three points, so if we don't have them, use the
+C     Lagrangian method instead
+      IF ( N.LE. 3 ) THEN
+        CALL LAGRAN(X,Yr,N,1,Xx,Yy,Iscal,1)
+        RETURN
+      ENDIF 
+
+C     Apply the scaling function
       DO i = 1 , N
         y(i) = FUNC(Yr(i),Iscal)
-c      PRINT*,x(i),yr(i),y(i),iscal
       ENDDO
-      
+
+C     Get the slope at each end
       yp1 = (y(2)-y(1))/(X(2)-X(1))
       ypn = (y(N)-y(N-1))/(X(N)-X(N-1))
+
+C     Fit a spline
       CALL SPLINE(X,y,N,yp1,ypn,w)
+
+C     Evaluate the spline at the desired point
       CALL SPLINT(X,y,w,N,Xx,ys)
+
+C     Apply the inverse scaling function
       Yy = FUNC1(ys,Iscal)
-c      PRINT * , 'Spline' , Xx , ys , Yy , Iscal
       RETURN
       END
  
