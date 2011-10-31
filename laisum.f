278444b2f64c65a6bd5cea564160217ef1821b67
Nigel Warr <warr@ikp.uni-koeln.de>
2011-10-31 08:49:19 +0100

    Comment

--- laisum.f
+++ laisum.f
@@ -111,8 +111,8 @@ C                    contribution
                            pamp = pamp1*ARM(is,I57) + pamp
                            IF ( ISO.EQ.0 .AND. rmis.GT..1 ) GOTO 10
                         ENDIF
-                        IF ( N.NE.m ) THEN
-                           irs = (-1)**(INT(rmir+rmis)-ISHA+iii) ! ISHA = 1 if half-integer spins
+                        IF ( N.NE.m ) THEN ! Same level
+                           irs = (-1)**(INT(rmir+rmis)-ISHA+iii) ! ISHA = 1 if half-integer spins, iii=0 for E, 1 for M
                            ARM(is,6) = ARM(is,6) + irs*pamp1*ARM(Ir,I57)
                            ISSTAR(i2) = MIN(is,ISSTAR(i2))
                            ISSTO(i2) = MAX(is,ISSTO(i2))
