warr/gosia

commit 22fbe1baa7129ccae60d2a20e1b737f46259f685

Re-evaluate a whole load of constants and make them double precision

qe.f
@@ -45,41 +45,45 @@ C Lmda = lambda (1 = E1, 2 = E2... 6 = E6)
       DIMENSION Cq(7)
 
       IF ( Lmda.EQ.2 ) THEN ! E2
-         Cq(1) = 0.75*(2.0*C2-D2)/B4*Pol
-         Cq(2) = -1.83711730*C*D/B4*Pol
-         Cq(3) = -0.91855865*D2/B4*Pol
+         Cq(1) = 3.D0/4.D0*(2.0D0*C2-D2)/B4*Pol
+         Cq(2) = -3.D0/2.D0*SQRT(3.D0/2.D0)*C*D/B4*Pol
+         Cq(3) = -3.D0/4.D0*SQRT(3.D0/2.D0)*D2/B4*Pol
          RETURN
       ELSEIF ( Lmda.EQ.3 ) THEN ! E3
-         Cq(1) = 1.875*C*(2.0*C2-3.0*D2)/B6
-         Cq(2) = -1.62379763*(4.0*C2-D2)*D/B6
-         Cq(3) = -5.13489890*C*D2/B6
-         Cq(4) = 2.09631373*D3/B6
+         Cq(1) = 15.D0/8.D0*C*(2.0D0*C2-3.0D0*D2)/B6
+         Cq(2) = -15.D0/16.D0*SQRT(3.D0)*(4.0D0*C2-D2)*D/B6
+         Cq(3) = -15.D0/8.D0*SQRT(15.D0/2.D0)*C*D2/B6
+         Cq(4) = 15.D0/16.D0*SQRT(5.D0)*D3/B6
          RETURN
       ELSEIF ( Lmda.EQ.4 ) THEN ! E4
-         Cq(1) = 1.09375000*(8.0*C4-24.0*C2*D2+3.0*D4)/B8
-         Cq(2) = -4.89139867*C*(4.0*C2-3.0*D2)*D/B8
-         Cq(3) = -3.45874113*(6.0*C2-D2)*D2/B8
-         Cq(4) = 12.9414244*C*D3/B8
-         Cq(5) = 4.57548440*D4/B8
+         Cq(1) = 35.D0/32.D0*(8.0D0*C4-24.0D0*C2*D2+3.0D0*D4)/B8
+         Cq(2) = -35.D0/16.D0*SQRT(5.D0)*C*(4.0D0*C2-3.0D0*D2)*D/B8
+         Cq(3) = -35.D0/15.D0*SQRT(5.D0/2.D0)*(6.0D0*C2-D2)*D2/B8
+         Cq(4) = 35.D0/16.D0*SQRT(35.D0)*C*D3/B8
+         Cq(5) = 35.D0/32.D0*SQRT(35.D0/2.D0)*D4/B8
          RETURN
       ELSEIF ( Lmda.EQ.5 ) THEN ! E5
-         Cq(1) = 1.230468*C*(-14.*C2*(9.*D2+B2)+30.*B4)/B10
-         Cq(2) = -1.347911*D*(35.*C2*(-3.*D2+B2)+5.*B4)/B10
-         Cq(3) = -35.662372*D2*C*(-3.*D2+2.*B2)/B10
-         Cq(4) = 7.279552*D3*(9.*C2-B2)/B10
-         Cq(5) = 30.884521*D4*C/B10
-         Cq(6) = -9.766543*D5/B10
+         Cq(1) = 315.D0/256.D0*C*(-14.D0*C2*(9.D0*D2+B2)+30.D0*B4)/B10
+         Cq(2) = -63.D0/256.D0*SQRT(30.D0)*D*(35.D0*C2*(-3.D0*D2+B2)+
+     &           5.D0*B4)/B10
+         Cq(3) = -315.D0/128.D0*SQRT(210.D0)*D2*C*(-3.D0*D2+2.D0*B2)/B10
+         Cq(4) = 315.D0/256.D0*SQRT(35.D0)*D3*(9.D0*C2-B2)/B10
+         Cq(5) = 945.D0/256.D0*SQRT(70.D0)*D4*C/B10
+         Cq(6) = -945.D0/256.D0*SQRT(7.D0)*D5/B10
          RETURN
       ELSEIF ( Lmda.EQ.6 ) THEN ! E6
-         Cq(1) = 2.707031*(21.*C2*(-C2*(11.*D2+4.*B2)+5.*B4)-5.*B6)/B12
-         Cq(2) = -17.543567*D*C*(3.*C2*(-11.*D2+B2)+5.*B4)/B12
-         Cq(3) = -13.869408*D2*(3.*C2*(-11.*D2+5.*B2)+B4)/B12
-         Cq(4) = 27.738815*D3*C*(-11.*D2+8.*B2)/B12
-         Cq(5) = 15.193177*D4*(11.*C2-B2)/B12
-         Cq(6) = -71.262308*D5*C/B12
-         Cq(7) = -20.571656*D6/B12
+         Cq(1) = 693.D0/256.D0*(21.D0*C2*(-C2*(11.D0*D2+4.D0*B2)+
+     &           5.D0*B4)-5.D0*B6)/B12
+         Cq(2) = -693.D0/256.D0*SQRT(42.D0)*D*C*(3.D0*C2*(-11.D0*D2+B2)+
+     &           5.D0*B4)/B12
+         Cq(3) = -693.D0/512.D0*SQRT(105.D0)*D2*(3.D0*C2*(-11.D0*D2+
+     &           5.D0*B2)+B4)/B12
+         Cq(4) = 693.D0/256.D0*SQRT(105.D0)*D3*C*(-11.D0*D2+8.D0*B2)/B12
+         Cq(5) = 2079.D0/512.D0*SQRT(14.D0)*D4*(11.D0*C2-B2)/B12
+         Cq(6) = -2079.D0/256.D0*SQRT(77.D0)*D5*C/B12
+         Cq(7) = -693.D0/512.D0*SQRT(231.D0)*D6/B12
          GOTO 99999
       ENDIF ! E1
-      Cq(1) = 0.5*C/B2
-      Cq(2) = -0.35355339*D/B2
+      Cq(1) = 1.D0/2.D0*C/B2
+      Cq(2) = -1.D0/2.D0/SQRT(2.D0)*D/B2
 99999 END

commit 473b780085579c9f6a6279274e353cf47c3d5bec

Fix error in E4 for qe introduced by commit 22fbe1baa7129ccae60d2a20e1b737f46259f685

qe.f
@@ -58,7 +58,7 @@ C Lmda = lambda (1 = E1, 2 = E2... 6 = E6)
       ELSEIF ( Lmda.EQ.4 ) THEN ! E4
          Cq(1) = 35.D0/32.D0*(8.0D0*C4-24.0D0*C2*D2+3.0D0*D4)/B8
          Cq(2) = -35.D0/16.D0*SQRT(5.D0)*C*(4.0D0*C2-3.0D0*D2)*D/B8
-         Cq(3) = -35.D0/15.D0*SQRT(5.D0/2.D0)*(6.0D0*C2-D2)*D2/B8
+         Cq(3) = -35.D0/16.D0*SQRT(5.D0/2.D0)*(6.0D0*C2-D2)*D2/B8
          Cq(4) = 35.D0/16.D0*SQRT(35.D0)*C*D3/B8
          Cq(5) = 35.D0/32.D0*SQRT(35.D0/2.D0)*D4/B8
          RETURN